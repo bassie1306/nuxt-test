2e432a846e07d7b1e662eeca3230750c6682096b
bassie1306 <112548205+bassie1306@users.noreply.github.com>
2024-02-14 08:37:02 +0100

    test

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,7 +2,7 @@
   <div
     class="px-8 mx-auto mt-12 prose sm:px-6 md:px-4 lg:px-2 xl:px-0 xl:prose-xl lg:prose-lg md:prose-md"
   >
-    <h2>Contact</h2>
+    <h2>test</h2>
     <form
       name="contact"
       method="post"
